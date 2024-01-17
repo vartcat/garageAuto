6603fd1d97ffe112e031ca5627244f9e97971ac1
catanasian <vartancatanasian@hotmail.fr>
2024-01-17 21:35:33 +0100

    navbar maj

--- app/views/home.php
+++ app/views/home.php
@@ -1,24 +1,6 @@
-<link rel="stylesheet" href="../../public/css/home.css">
+<?=template_header('')?>
 
-<nav class="navbar">
-    <div class="navbar-container container">
-        <input type="checkbox" name="" id="">
-        <div class="hamburger-lines">
-            <span class="line line1"></span>
-            <span class="line line2"></span>
-            <span class="line line3"></span>
-        </div>
-        <ul class="menu-items">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Category</a></li>
-            <li><a href="#">Menu</a></li>
-            <li><a href="#">Testimonial</a></li>
-            <li><a href="#">Contact</a></li>
-        </ul>
-        <h1 class="logo">Navbar</h1>
-    </div>
-</nav>
+<link href="../../public/css/home.css" rel="stylesheet" type="text/css">
 
 <body>
     <h1 class="">Welcome to the Home Page</h1>
--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -1,9 +1,41 @@
+<?php
+
+function template_header($title) {
+    echo <<<EOT
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $title ?></title>
-    <link rel="stylesheet" href="<?= BASEURL ?>/css/style.css">
-</head>
-<body>
+    <link href="../../../public/css/style.css" rel="stylesheet" type="text/css">
+    </head>
+<nav class="navbar">
+    <div class="navbar-container container">
+        <input type="checkbox" name="" id="">
+        <div class="hamburger-lines">
+            <span class="line line1"></span>
+            <span class="line line2"></span>
+            <span class="line line3"></span>
+        </div>
+        <ul class="menu-items">
+            <li><a href="../home.php">Garage</a></li>
+            <li><a href="../services.php">Prestations</a></li>
+            <li><a href="../occasions.php">Occasions</a></li>
+            <li><a href="../notices.php">Avis</a></li>
+            <li><a href="../contact.php">Contact</a></li>
+            <li><a href="../login.phps">Logins</a></li>
+        </ul>
+        <h1 class="logo">Garage V.Parrot</h1>
+    </div>
+</nav>
+EOT;
+}
+function template_footer() {
+echo <<<EOT
+    </body>
+</html>
+EOT;
+}
+?>
